d60af5294e690570378c1ecd6c43b7a4d0f8f6ce
Szymon Świędrych <szymek_sw@outlook.com>
2022-03-31 12:47:25 +0200

    Fix Draw Spirte function

--- lib/model/include/sprites.h
+++ lib/model/include/sprites.h
@@ -4,15 +4,12 @@
 #include "stm32f1xx.h"
 
 const extern uint16_t sprite_snake_head_horizontal[];
-
-// 'sprite_logo', 32x32px
 const extern uint16_t sprite_logo[];
 const extern uint16_t sprite_apple[];
 const extern uint16_t sprite_snakeTail[];
 const extern uint16_t sprite_snake_head_eating_horizontal[];
 const extern uint16_t sprite_snakeHorizontal[];
 const extern uint16_t sprite_snakeCorner[];
-const extern uint16_t sprite_snake_head_horizontal[];
 const extern uint16_t sprite_snakeVertical[];
 const extern uint16_t sprite_snake_head_eating_vertical[];
 const extern uint16_t sprite_snake_head_vertical[];
