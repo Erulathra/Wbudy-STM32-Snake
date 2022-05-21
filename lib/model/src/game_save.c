74383e0dfe4ab85d29f8a9bb6dca39d63ddb92a8
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-05-21 21:54:01 +0200

    Minor bugfixes in game_save

--- lib/model/src/game_save.c
+++ lib/model/src/game_save.c
@@ -5,12 +5,12 @@
 #include "game_save.h"
 #include "game_engine.h"
 
-void save_game(Snake &savedSnake, Apple &savedApple, uint8_t &savedTali[16])
+void save_game(Snake &savedSnake, Apple &savedApple, uint8_t *savedTail[16])
 {
     //TODO:save to Flash
 }
 
-void save_game(Snake *savedSnake, Apple *savedApple, uint8_t *savedTali[16])
+void load_game(Snake *savedSnake, Apple *savedApple, uint8_t *savedTail[16])
 {
     //TODO:load from Flash
 }
