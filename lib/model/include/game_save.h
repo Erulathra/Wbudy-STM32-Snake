dc350b8c8ab0a5dc389d45d2005b109a4d60e9d4
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-05-21 20:00:39 +0200

    Add dummy functions for game save

---
+++ lib/model/include/game_save.h
@@ -0,0 +1,8 @@
+//
+// Created by MasterKtos on 21.05.2022.
+//
+
+#ifndef WBUDY_STM32_SNAKE_GAME_SAVE_H
+#define WBUDY_STM32_SNAKE_GAME_SAVE_H
+
+#endif //WBUDY_STM32_SNAKE_GAME_SAVE_H
---
+++ lib/model/src/game_save.cpp
@@ -0,0 +1,16 @@
+//
+// Created by MasterKtos on 21.05.2022.
+//
+
+#include "game_save.h"
+#include "game_engine.h"
+
+void save_game(Snake &savedSnake, Apple &savedApple, uint8_t &savedTali[16])
+{
+    //TODO:save to Flash
+}
+
+void save_game(Snake *savedSnake, Apple *savedApple, uint8_t *savedTali[16])
+{
+    //TODO:load from Flash
+}
