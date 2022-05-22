30f444c497c1f335b91f4093ac330d570f725aec
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-05-22 19:00:24 +0200

    Add save and load functions

--- lib/model/include/game_save.h
+++ lib/model/include/game_save.h
@@ -5,10 +5,12 @@
 #ifndef WBUDY_STM32_SNAKE_GAME_SAVE_H
 #define WBUDY_STM32_SNAKE_GAME_SAVE_H
 
+#define HALF_WORD_LENGTH (uint32_t) 0x00000010
+
 #include "game_engine.h"
 #include "eeprom.h"
 
-void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail[16]);
-void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail[16]);
+void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail);
+void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail);
 
 #endif //WBUDY_STM32_SNAKE_GAME_SAVE_H
--- lib/model/src/game_save.c
+++ lib/model/src/game_save.c
@@ -4,12 +4,87 @@
 
 #include "game_save.h"
 
-void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail[16])
+//  Combine uint8_t variables to one uint16_t variable
+// to make saving more efficient.
+//  It does it in such a way that, given two variables (in binary format):
+//      uint8_t one = xxxxxxxx(b);
+//      uint8_t two = yyyyyyyy(b);
+// outputs:
+//      uint16_t one_two = xxxxxxxxyyyyyy(b);
+#define combine_uint_16t(x, y) ((x << 8) | y)
+// Provides mask of given size for reading from combined value.
+// How it works:
+//      res = 1 >> 8 (100000000(b))
+//      res - 1 = 11111111(b)
+#define mask(x) ((1 << x) - 1)
+
+void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail)
 {
-    //TODO:save to Flash
+    //Snake = 4 words
+    //Apple = 3 words
+    //tail = 16 words
+    uint32_t currentAddress = EEPROM_START_ADDRESS;
+
+    enableEEPROMWriting();
+
+    // save Snake
+    writeEEPROMHalfWord(currentAddress,
+                        combine_uint_16t(savedSnake->x, savedSnake->y));
+    currentAddress += HALF_WORD_LENGTH;
+
+    writeEEPROMHalfWord(currentAddress,
+                        combine_uint_16t(savedSnake->direction, savedSnake->tailLength));
+    currentAddress += HALF_WORD_LENGTH;
+
+    // save Apple
+    writeEEPROMHalfWord(currentAddress,
+                        combine_uint_16t(savedApple->x, savedApple->y));
+    currentAddress += HALF_WORD_LENGTH;
+
+    //save Tail
+    for(uint8_t i = 0; i < SEGMENT_SIZE; i+=2, savedTail++) {
+        uint8_t prev_val = *savedTail;
+        uint8_t next_value = *(++savedTail);
+        writeEEPROMHalfWord(currentAddress,
+                            combine_uint_16t(prev_val, next_value));
+        currentAddress += HALF_WORD_LENGTH;
+    }
+
+    // TODO:write checksum
+
+    disableEEPROMWriting();
 }
 
-void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail[16])
+void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail)
 {
-    //TODO:load from Flash
+    uint32_t currentAddress = EEPROM_START_ADDRESS;
+
+    //TODO:check checksum
+
+    // read Snake
+    uint16_t snake_x_y = readEEPROMHalfWord(currentAddress);
+    // bitwise shift gets rid of last 8 bytes
+    savedSnake->x = (uint8_t) snake_x_y >> 8;
+    savedSnake->y = snake_x_y & mask(8);
+    currentAddress += HALF_WORD_LENGTH;
+
+    uint16_t snake_dir_len = readEEPROMHalfWord(currentAddress);
+    savedSnake->direction = (uint8_t) snake_dir_len >> 8;
+    savedSnake->tailLength = snake_x_y & mask(8);
+    currentAddress += HALF_WORD_LENGTH;
+
+    // read Apple
+    uint16_t apple_x_y = readEEPROMHalfWord(currentAddress);
+    savedApple->x = (uint8_t) apple_x_y >> 8;
+    savedApple->y = apple_x_y & mask(8);
+    currentAddress += HALF_WORD_LENGTH;
+
+    // read Tail
+    for(uint8_t i = 0; i < SEGMENT_SIZE; i+=2, savedTail++) {
+        uint16_t two_segments = readEEPROMHalfWord(currentAddress);
+        //TODO: check in online compiler
+        *savedTail = (uint8_t) (two_segments >> 8);
+        *(++savedTail) = two_segments & mask(8);
+        currentAddress += HALF_WORD_LENGTH;
+    }
 }
