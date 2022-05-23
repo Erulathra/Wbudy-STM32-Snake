72c9775876cccab5996446f86605bb13ea1deaa2
Michalina Śledzikowska <236669@edu.p.lodz.pl>
2022-05-23 17:51:47 +0200

    Add checksum

--- lib/model/include/game_save.h
+++ lib/model/include/game_save.h
@@ -11,6 +11,8 @@
 #include "eeprom.h"
 
 void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail);
-void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail);
+uint8_t load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail);
+uint8_t ones(uint8_t number);
+uint8_t do_checksum(uint8_t calc_checksum, uint16_t read_checksum);
 
 #endif //WBUDY_STM32_SNAKE_GAME_SAVE_H
--- lib/model/src/game_save.c
+++ lib/model/src/game_save.c
@@ -25,6 +25,7 @@ void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *save
     //Apple = 3 words
     //tail = 16 words
     uint32_t currentAddress = 0;
+    uint8_t checksum = 0;
 
     enableEEPROMWriting();
 
@@ -32,15 +33,18 @@ void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *save
     writeEEPROMHalfWord(currentAddress,
                         combine_uint_16t(savedSnake->x, savedSnake->y));
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedSnake->x) + ones(savedSnake->y);
 
     writeEEPROMHalfWord(currentAddress,
                         combine_uint_16t(savedSnake->direction, savedSnake->tailLength));
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedSnake->direction) + ones(savedSnake->tailLength);
 
     // save Apple
     writeEEPROMHalfWord(currentAddress,
                         combine_uint_16t(savedApple->x, savedApple->y));
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedApple->x) + ones(savedApple->y);
 
     //save Tail
     for(uint8_t i = 0; i < SEGMENT_SIZE; i+=2, savedTail++) {
@@ -49,18 +53,20 @@ void save_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *save
         writeEEPROMHalfWord(currentAddress,
                             combine_uint_16t(prev_val, next_value));
         currentAddress += HALF_WORD_LENGTH;
+        checksum += ones(prev_val) + ones(next_value);
     }
 
-    // TODO:write checksum
+    //write checksum
+    writeEEPROMHalfWord(currentAddress,checksum);
 
     disableEEPROMWriting();
 }
 
-void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail)
+// returns 0 if game failed to load, 1 otherwise
+uint8_t load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *savedTail)
 {
     uint32_t currentAddress = 0;
-
-    //TODO:check checksum
+    uint8_t checksum = 0;
 
     // read Snake
     uint16_t snake_x_y = readEEPROMHalfWord(currentAddress);
@@ -69,24 +75,45 @@ void load_game(struct Snake *savedSnake, struct Apple *savedApple, uint8_t *save
     savedSnake->x = (uint16_t) snake_x_y >> 8;
     savedSnake->y = snake_x_y & mask(8);
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedSnake->x) + ones(savedSnake->y);
 
     uint16_t snake_dir_len = readEEPROMHalfWord(currentAddress);
     savedSnake->direction = (uint16_t) snake_dir_len >> 8;
     savedSnake->tailLength = snake_dir_len & mask(8);
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedSnake->direction) + ones(savedSnake->tailLength);
 
     // read Apple
     uint16_t apple_x_y = readEEPROMHalfWord(currentAddress);
     savedApple->x = (uint16_t) apple_x_y >> 8;
     savedApple->y = apple_x_y & mask(8);
     currentAddress += HALF_WORD_LENGTH;
+    checksum += ones(savedApple->x) + ones(savedApple->y);
 
     // read Tail
     for(uint8_t i = 0; i < SEGMENT_SIZE; i+=2, savedTail++) {
         uint16_t two_segments = readEEPROMHalfWord(currentAddress);
         //TODO: check in online compiler
         *savedTail = (uint16_t) (two_segments >> 8);
+        checksum += ones(*savedTail);
         *(++savedTail) = two_segments & mask(8);
+        checksum += ones(*savedTail);
         currentAddress += HALF_WORD_LENGTH;
     }
+
+    return do_checksum(checksum, readEEPROMHalfWord(currentAddress));
+}
+
+uint8_t ones(uint8_t number) {
+    uint8_t result = 0;
+    for(;number > 0; number >>= 1) {
+        result += number & 1;
+    }
+    return result;
+}
+
+// returns 1 if checksum is correct, 0 otherwise
+uint8_t do_checksum(uint8_t calc_checksum, uint16_t read_checksum) {
+    if((uint16_t) calc_checksum == read_checksum) return 1;
+    return 0;
 }
